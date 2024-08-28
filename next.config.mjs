8517f8d51568228a67915492430eecbeb306a12c
Flavio150303 <168148503+Flavio150303@users.noreply.github.com>
2024-08-28 02:11:45 -0500

    aeaAyudame

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,  // Activa el modo estricto de React para detectar problemas potenciales
+    swcMinify: true,        // Habilita la minificación con SWC para mejorar el rendimiento
+  
+    // Configurar redirecciones
+    async redirects() {
+      return [
+        {
+          source: '/old-route',
+          destination: '/new-route',
+          permanent: true,   // Redirección permanente (código de estado 308)
+        },
+      ];
+    },
+    experimental: {
+        appDir: true,  // Habilita el uso de la carpeta `src/app`
+      },
+    
+  };
+  
+  export default nextConfig;
+  
